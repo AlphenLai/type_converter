895836dce61c1442d7768d5126f5cc076f505f41
Alvin <44488429+AlphenLai@users.noreply.github.com>
2019-07-19 16:41:01 +0800

    Updated codes

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,15 @@
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/PoseStamped.h>
 
+static double current_x = 0.0;
+static double current_y = 0.0;
+static double current_z = 0.0;
+static geometry_msgs::Quaternion current_q;
+
 void local_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg);
 
 int main(int argc, char **argv)
 {
-  
   ros::init(argc, argv,"type_converter");
   ros::NodeHandle n;
 
@@ -19,14 +23,16 @@ int main(int argc, char **argv)
   tf::TransformListener listener;
 
   //read local_position pose and convert it to tf
+  static tf::TransformBroadcaster br;
   ros::Subscriber local_pos_pose = n.subscribe("/mavros/local_position/pose",1,local_pose_callback);
 
+  ros::Time current_time = ros::Time::now();
   while (n.ok())
   {
     tf::StampedTransform transform;
     try
     {
-      listener.waitForTransform("camera_position", "world", ros::Time(0), ros::Duration(10.0) );
+      listener.waitForTransform("camera_position", "world", ros::Time(0), ros::Duration(0.5) );
       listener.lookupTransform("camera_position", "world", ros::Time(0), transform);
     }
     catch(tf::TransformException ex)
@@ -34,19 +40,45 @@ int main(int argc, char **argv)
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
     }
-    
-    geometry_msgs::PoseStamped vision_pos_pose;
-    vision_pos_pose.pose.orientation.x = transform.getRotation().getX();
-    vision_pos_pose.pose.orientation.y = transform.getRotation().getY();
-    vision_pos_pose.pose.orientation.z = transform.getRotation().getZ();
-    vision_pos_pose.pose.orientation.w = transform.getRotation().getW();
 
-    vision_pos_pose.pose.position.x = transform.getOrigin().getX();
-    vision_pos_pose.pose.position.y = transform.getOrigin().getY();
-    vision_pos_pose.pose.position.z = transform.getOrigin().getZ();
+    //convert poseStamped from stampedTF
+    geometry_msgs::PoseStamped vision_pos_pose;
+    //convert orientaiton
+    geometry_msgs::Quaternion quat;
+    tf::Quaternion tfQuat = transform.getRotation();
+    quat.x = tfQuat.x();
+    quat.y = tfQuat.y();
+    quat.z = tfQuat.z();
+    quat.w = tfQuat.w();
+    vision_pos_pose.pose.orientation = quat;
+
+    //convert position
+    tf::Vector3 tfVec = transform.getOrigin();
+    geometry_msgs::Point pt;
+    pt.x = tfVec.getX();
+    pt.y = tfVec.getY();
+    pt.z = tfVec.getZ();
+    vision_pos_pose.pose.position = pt;
+
+    //convert header
+    vision_pos_pose.header.frame_id = transform.frame_id_;
+    vision_pos_pose.header.stamp = transform.stamp_;
 
     marker_pose.publish(vision_pos_pose);
   }
+
+  geometry_msgs::TransformStamped local_pose_tf;
+  local_pose_tf.transform.translation.x = current_x;
+  local_pose_tf.transform.translation.y = current_y;
+  local_pose_tf.transform.translation.z = current_z;
+  local_pose_tf.transform.rotation = current_q;
+
+  //header
+  local_pose_tf.header.stamp = current_time;
+  local_pose_tf.header.frame_id = "world";
+  local_pose_tf.child_frame_id = "robot_link";
+
+  br.sendTransform(local_pose_tf);
 
   ros::spin();
   return 0;
@@ -54,9 +86,8 @@ int main(int argc, char **argv)
 
 void local_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg)
 {
-  static tf::TransformBroadcaster br;
-  tf::Transform local_pose_tf;
-  local_pose_tf.setOrigin( tf::Vector3(pose_msg->pose.position.x, pose_msg->pose.position.y, pose_msg->pose.position.z) );
-  local_pose_tf.setRotation( tf::Quaternion(pose_msg->pose.orientation.x, pose_msg->pose.orientation.y, pose_msg->pose.orientation.z, pose_msg->pose.orientation.w) );
-  br.sendTransform(tf::StampedTransform(local_pose_tf, ros::Time::now(),"world", "local_position"));
+  current_x = pose_msg->pose.position.x;
+  current_y = pose_msg->pose.position.y;
+  current_z = pose_msg->pose.position.z;
+  current_q = pose_msg->pose.orientation;
 }
